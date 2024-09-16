d83c9c4c03dc9406c28aedc783310be67857cc7b
JanCieszynski <janekcieszynski1@gmail.com>
2024-09-16 10:37:46 +0200

    zdjecia

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -14,9 +14,25 @@
             Random r = new Random();
             int jancieszynski = r.Next(1,7);
             rollValueLabel.Text = "Wylosowany rzut: " + jancieszynski.ToString();
-
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        private void OnCounterClicked1(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int jancieszynski = r.Next(1, 5);
+            rollValueLabel.Text = "Wylosowany rzut: " + jancieszynski.ToString();
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnCounterClicked3(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int jancieszynski = r.Next(1, 11);
+            rollValueLabel.Text = "Wylosowany rzut: " + jancieszynski.ToString();
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
     }
 
 }
